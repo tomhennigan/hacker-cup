11da966329c24196c944d675ce4f212455293e1e
Tom Hennigan <tomhennigan@gmail.com>
2013-02-02 18:26:03 +0000

    Failed at 20-card-game.

---
+++ round1/20-card-game/solution.py
@@ -0,0 +1,34 @@
+import itertools
+
+def read_file(input_file):
+    with open(input_file, 'r') as fp:
+        num_lines = int(fp.next())
+        # print "num_lines: %d" % num_lines
+
+        while True:
+            try:
+                N, K = map(int, fp.next().split())
+                a = map(int, fp.next().split())
+
+                yield N, K, a
+
+            except StopIteration as e:
+                return
+
+
+def solution(N, K, a):
+    # print 'N=%r, K=%r, a=%r' % (N, K, a)
+    # N == len(a)
+    # K == subset_size
+    # a[i] % 1000000007
+    # map(lambda a: a % 1000000007)
+
+    the_sum = 0
+    for combination in itertools.combinations(a, K):
+        the_sum += max(combination)
+        the_sum %= 1000000007
+
+    return the_sum
+
+for case_num, (N, K, a) in enumerate(read_file('input.txt'), start=1):
+    print 'Case #%d: %d' % (case_num, solution(N, K, a))
