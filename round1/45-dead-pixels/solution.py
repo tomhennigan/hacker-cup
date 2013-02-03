8bfcef220de52ba863de4d65509f9fde49dd84c4
Tom Hennigan <tomhennigan@gmail.com>
2013-02-03 10:21:01 +0000

    Tidy up solution a little.

--- round1/45-dead-pixels/solution.py
+++ round1/45-dead-pixels/solution.py
@@ -23,25 +23,9 @@ def read_file(input_file):
 def solution(W, H, P, Q, N, X, Y, a, b, c, d):
     # print 'W=%r, H=%r, P=%r, Q=%r, N=%r, X=%r, Y=%r, a=%r, b=%r, c=%r, d=%r' % (W, H, P, Q, N, X, Y, a, b, c, d)
 
-    # x[0] = X
-    # y[0] = Y
-    # x[i] = (x[i - 1] * a + y[i - 1] * b + 1) % W (for 0 < i < N)
-    # y[i] = (x[i - 1] * c + y[i - 1] * d + 1) % H (for 0 < i < N)
-
+    # Initialise dead pixel locations.
     dead_pixels = set()
-
-    # x = [None] * N
-    # y = [None] * N
-    # x[0] = X
-    # y[0] = Y
-    # dead_pixels.add((x[0], y[0]))
-    # for i in xrange(1, N):
-    #     x[i] = (x[i - 1] * a + y[i - 1] * b + 1) % W
-    #     y[i] = (x[i - 1] * c + y[i - 1] * d + 1) % H
-    #     dead_pixels.add((x[i], y[i]))
-
-    prev_x = X
-    prev_y = Y
+    prev_x, prev_y = X, Y
     dead_pixels.add((prev_x, prev_y))
     for i in xrange(1, N):
         x = (prev_x * a + prev_y * b + 1) % W
@@ -51,9 +35,8 @@ def solution(W, H, P, Q, N, X, Y, a, b, c, d):
 
     # print 'dead_pixels=%r' % sorted(dead_pixels)
 
-    # Valid offsets
+    # Valid screen offsets to place the image.
     offsets = set()
-
     for y_start in xrange(0, (H - Q) + 1):
         for x_start in xrange(0, (W - P) + 1):
             contains_dead_pixel = False
@@ -73,29 +56,6 @@ def solution(W, H, P, Q, N, X, Y, a, b, c, d):
                 offset = (x_start, y_start)
                 # print 'offset=%r' % (offset, )
                 offsets.add(offset)
-
-    # print the screen
-    # print
-    # import sys
-    # for y in xrange(0, H):
-    #     sys.stdout.write('    ')
-    #     if y < 0xf:
-    #         sys.stdout.write('%X ' % y)
-    #     else:
-    #         sys.stdout.write('  ')
-
-    #     for x in xrange(0, W):
-    #         pixel = (x, y)
-    #         sys.stdout.write('|')
-    #         if pixel in dead_pixels:
-    #             sys.stdout.write('D')
-    #         elif pixel in offsets:
-    #             sys.stdout.write('o')
-    #         else:
-    #             sys.stdout.write(' ')
-
-    #     sys.stdout.write('|\n')
-    # print
 
     # print 'offsets=%r' % sorted(offsets)
     positions = len(offsets)
